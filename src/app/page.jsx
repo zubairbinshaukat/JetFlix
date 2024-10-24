126d28e0bbb26dae450c1aef11fc06daa02f04cc
Zubair Bin Shaukat <145450776+zubairbinshaukat@users.noreply.github.com>
2024-10-25 02:12:01 +0500

    minor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 export default function Home() {
   return (
-    <div className="">
+    <div className="w-full h-screen">
       {/* <div className="text-5xl text-white backdrop-blur bg-black/10 p-6 rounded-3xl"> Hello World!</div> */}
       
     </div>
