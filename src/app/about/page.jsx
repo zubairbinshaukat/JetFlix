c89df9773f205da210f3b5636bb0a170a091b5b7
zubairbinshaukat <zubairbinshaukat4455@gmail.com>
2024-10-25 02:08:00 +0500

    navbar

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full text-5xl h-screen flex items-center justify-center">
+      About Page
+    </div>
+  );
+};
+
+export default page;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,30 @@
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import Splash from '@/components/Splash'
+import MobileNavbar from '@/components/MobileNavbar'
 
 export const metadata = {
-  title: "Nazara",
-  description: "A Movies and Web Series Streaming App!",
-};
+  title: 'Nazara',
+  description: 'A Movies and Web Series Streaming App!',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-[url("https://images.unsplash.com/photo-1548320605-e74e368ff59c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGRhcmslMjBmb3Jlc3R8ZW58MHx8MHx8fDA%3D")]
+        dark:bg-[url("https://images.pexels.com/photos/716834/pexels-photo-716834.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] 
+        bg-no-repeat bg-cover antialiased`}
       >
-        {children}
+        <div className="w-full bg-white/10 backdrop-filter backdrop-blur-2xl backdrop-contrast-25 h-full">
+          <div className="max-w-[1300px] mx-auto">
+            <Splash />
+            <Navbar />
+            {/* <MobileNavbar/> */}
+            {children}
+          </div>
+        </div>
       </body>
     </html>
-  );
+  )
 }
