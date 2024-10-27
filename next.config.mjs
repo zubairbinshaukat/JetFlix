4d47d543c3209af66331177ca3b378f319e8de30
zubairbinshaukat <zubairbinshaukat4455@gmail.com>
2024-10-27 20:12:11 +0500

    pwa check

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+
+});
 
 export default nextConfig;
+
+
+// const withPWA = require('next-pwa')({
+//     dest: 'public', // Output directory for service worker files
+//     register: true, // Register the service worker automatically
+//     skipWaiting: true, // Update the service worker immediately without waiting for users to close tabs
+//   });
+  
+//   /** @type {import('next').NextConfig} */
+//   const nextConfig = withPWA({
+//     reactStrictMode: true,
+//   });
+  
+//   module.exports = nextConfig;
+  
